4639fa032efe08f7aac37a910720c3dad32533cb
Melanie Cardenas <melaniecardenas19@yahoo.com>
2023-11-02 00:10:44 +0000

    C125 LargeestSum List<Integer> list = nums; int maxSum = Integer.MIN_VALUE; for (i) for (j)

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -10,6 +10,19 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        List<Integer> list = nums;
+        
+        int maxSum = Integer.MIN_VALUE;
+
+        for(int i =0; i < list.size(); i++){
+            for(int j= i +1; j < list.size(); j++){
+                int sum = list.get(i) + list.get(j);
+                if(sum > maxSum){
+                    maxSum = sum;
+                }
+            }
+            
+        }
+        return maxSum;
     }
 }
